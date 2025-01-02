0603fd49c07486808e250691f2013d4e82409d7e
bawuj <bawujdevchain@gmail.com>
2025-01-02 08:01:08 +0100

    config2

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-icons/fa'],
-    },
-  },
+
 })
